8859ea83ce7f89dc507ff6a0f9cbcd4d9f1ea7d9
EricLeeN1 <179310009@qq.com>
2022-02-08 08:32:32 +0800

    feac:增加normalize

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import router from "./router/";
 import { createPinia } from "pinia";
 import ElementPlus from "element-plus";
+import "normalize.css";
+
 import "element-plus/dist/index.css";
 const store = createPinia();
 const app = createApp(App);
