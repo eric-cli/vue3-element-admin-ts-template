587cd0aea3bf8fe9596e338aa2e1462c88325721
EricLeeN1 <179310009@qq.com>
2022-02-25 13:10:52 +0800

    feac:增加行内修改table

---
+++ src/views/Table/inline-edit-table.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="app-container">
+    <el-table
+      v-loading="listLoading"
+      :data="list"
+      border
+      fit
+      highlight-current-row
+      style="width: 100%"
+    >
+      <el-table-column align="center" label="ID" width="80">
+        <template #default="{ row }">
+          <span>{{ row.id }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column width="180px" align="center" label="Date">
+        <template #default="{ row }">
+          <span>{{ parseTime(row.timestamp, "{y}-{m}-{d} {h}:{i}") }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column width="120px" align="center" label="Author">
+        <template #default="{ row }">
+          <span>{{ row.author }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column width="100px" label="Importance">
+        <template #default="{ row }">
+          <el-row align="middle">
+            <svg-icon
+              v-for="n in +row.importance"
+              :key="n"
+              icon-class="star"
+              class="meta-item__icon"
+            />
+          </el-row>
+        </template>
+      </el-table-column>
+
+      <el-table-column class-name="status-col" label="Status" width="110">
+        <template #default="{ row }">
+          <el-tag :type="statusFilter(row.status)">
+            {{ row.status }}
+          </el-tag>
+        </template>
+      </el-table-column>
+
+      <el-table-column min-width="300px" label="Title">
+        <template #default="{ row }">
+          <template v-if="row.edit">
+            <el-input v-model="row.title" class="edit-input" size="small" />
+            <el-button
+              class="cancel-btn"
+              size="small"
+              :icon="Refresh"
+              type="warning"
+              @click="cancelEdit(row)"
+            >
+              cancel
+            </el-button>
+          </template>
+          <span v-else>{{ row.title }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column align="center" label="Actions" width="120">
+        <template #default="{ row }">
+          <el-button
+            v-if="row.edit"
+            type="success"
+            size="small"
+            :icon="CircleCheck"
+            @click="confirmEdit(row)"
+          >
+            Ok
+          </el-button>
+          <el-button
+            v-else
+            type="primary"
+            size="small"
+            :icon="Edit"
+            @click="row.edit = !row.edit"
+          >
+            Edit
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Refresh, Edit, CircleCheck } from "@element-plus/icons-vue";
+import { fetchList } from "@/apis/article";
+import { parseTime } from "@/utils";
+const statusFilter = (status) => {
+  const statusMap = {
+    published: "success",
+    draft: "info",
+    deleted: "danger",
+  };
+  return statusMap[status];
+};
+const list = ref([]);
+const listLoading = ref(true);
+const listQuery = ref({
+  page: 1,
+  limit: 10,
+});
+const getList = async () => {
+  listLoading.value = true;
+  const { data } = await fetchList(listQuery.value);
+  const items = data.lists;
+  list.value = items.map((v) => {
+    // this.$set(v, "edit", false); // https://vuejs.org/v2/guide/reactivity.html
+    v.edit = false;
+    v.originalTitle = v.title; //  will be used when user click the cancel botton
+    return v;
+  });
+  listLoading.value = false;
+};
+const cancelEdit = (row) => {
+  row.title = row.originalTitle;
+  row.edit = false;
+  ElMessage({
+    message: "The title has been restored to the original value",
+    type: "warning",
+  });
+};
+const confirmEdit = (row) => {
+  row.edit = false;
+  row.originalTitle = row.title;
+  ElMessage({
+    message: "The title has been edited",
+    type: "success",
+  });
+};
+onMounted(() => {
+  getList();
+});
+</script>
+
+<style lang="scss" scoped>
+.edit-input {
+  padding-right: 100px;
+}
+.cancel-btn {
+  position: absolute;
+  right: 15px;
+  top: 10px;
+}
+</style>
